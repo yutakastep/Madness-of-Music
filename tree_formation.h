4b14970eccb329a35c42a98d55cae3efb3d0fbe7
yutakastep <127261743+yutakastep@users.noreply.github.com>
2024-04-20 22:33:23 -0400

    Update tree_formation.h

--- tree_formation.h
+++ tree_formation.h
@@ -6,43 +6,6 @@
 #include <sstream>
 using namespace std;
 
-// Node creation
-//class Node
-//{
-//public:
-//    int val;
-//
-//    // Since n children are possible for a root.
-//    // A list created to store all the children.
-//    vector<Node *> child;
-//
-//    // Constructor
-//    Node(int data) : val(data) {}
-//};
-//
-//void insert (Node* root, int parent, Node* node) {
-//    if (!root) {
-//        root = node;
-//    } else {
-//        if (root->val == parent) {
-//            root->child.push_back(node);
-//        } else {
-//            // Recursive approach to
-//            // insert the child
-//            int l = root->child.size();
-//
-//            for(int i = 0; i < l; i++) {
-//                if (root->child[i]->val == parent) {
-//                    insert(root->child[i], parent, node);
-//                } else {
-//                    insert(root->child[i], parent, node);
-//                }
-//            }
-//        }
-//    }
-//}
-//
-
 class TreeNode {
 public:
     int data;
