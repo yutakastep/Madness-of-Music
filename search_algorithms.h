57fb14a64152fc37ba1c8281a364c1b28cec02cb
andrethehbk <100052610+andrethehbk@users.noreply.github.com>
2024-04-22 17:25:21 -0400

    Update search_algorithms.h
    added 3 functions: printNodes, DFS recursive algorithm, and a main DFS function to retain clock functionality

--- search_algorithms.h
+++ search_algorithms.h
@@ -2,6 +2,7 @@
 #include <iomanip>
 #include <ctime>
 #include <queue>
+#include <stack>
 
 using namespace std;
 
@@ -12,16 +13,20 @@ pair<vector<Node*>, double> BFS(Node* root, unsigned maxChildren = 10) {
     int levelNum = 1;
     if (root == nullptr) return {children, 0}; // returns empty if root is empty
     queue.push(root);
+
     while (!queue.empty()) {
         int size = queue.size();
         vector<int> level;
+
         for (int i = 0; i < size; i++) {
             Node* node = queue.front();
             level.push_back(node->val);
             queue.pop();
-            for (int j = 0; j < (node->children).size(); j++) {
-                if ((node->children)[j] != nullptr) {
-                    queue.push((node->children)[j]);
+            for (Node* child : node->children) {
+                if (child != nullptr) {
+                    queue.push(child);
+                    if (children.size() < maxChildren) // collect up to maxChildren nodes to print out
+                        children.push_back(child);
                 }
             }
         }
@@ -29,9 +34,36 @@ pair<vector<Node*>, double> BFS(Node* root, unsigned maxChildren = 10) {
     }
     clock_t end = clock(); // record the end time
     double elapsed = double(end - start) / CLOCKS_PER_SEC; // calculate elapsed time
+
     return {children, elapsed};
 }
 
-pair<vector<Node*>, double> DFS(Node* root, unsigned maxChildren = 10) {
+void dfsRecursion(Node* node, vector<Node*>& children, unsigned maxChildren) {
+    if (node == nullptr) return;
 
+    if (children.size() < maxChildren) children.push_back(node);
+
+    for (Node* child : node->children) {
+        dfsRecursion(child, children, maxChildren);
+    }
+}
+
+pair<vector<Node*>, double> DFS(Node* root, unsigned maxChildren = 10) {
+    clock_t start = clock(); // record the start time
+
+    vector<Node*> children;
+    dfsRecursion(root, children, maxChildren); // recursive DFS function
+
+    clock_t end = clock(); // record the end time
+    double elapsed = double(end - start) / CLOCKS_PER_SEC; // calculate elapsed time
+
+    return {children, elapsed};
+}
+
+void printNodes(const vector<Node*>& nodes) {
+    cout << "Children nodes: ";
+    for (const Node* node : nodes) {
+        cout << node->val << " ";
+    }
+    cout << endl;
 }
